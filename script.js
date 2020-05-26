3c5d8ce70864c4b6e0f9c58df5b3c2b15a3100c9
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-05-25 23:40:42 -0500

    update js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// $(document).ready(function () {
-//     //alert("is JS file linked?");
-//     //TODO Setup variabes here
-// });
-console.log("js")
-var search = $("#search")
-var searchSubmit = $("#searchSubmit")
-var searchClear = $("#searchClear")
+$(document).ready(function () {
+    //     //TODO Setup variabes here
+    // 
+    console.log("js")
+    var search = $("#search")
+    var searchSubmit = $("#searchSubmit")
+    var searchClear = $("#searchClear")
 
-var apiKey = "Wnzr87egH7NGFAHMg3rOj8yG4AgnCejd"
+    var apiKey = "Wnzr87egH7NGFAHMg3rOj8yG4AgnCejd"
 
-// $(document).ready(function () {
-//     $('.datepicker').datepicker();
-// });
+    // $(document).ready(function () {
+    //     $('#begin-date').datepicker();
+    // });
 
 
-searchSubmit.on("click", function (event) {
-    console.log(search.val())
-    getNYCarticles(search.val())
-})
+    searchSubmit.on("click", function (event) {
+        console.log(search.val())
+        getNYTarticles(search.val())
+    })
 
-function getNYCarticles(val) {
-    var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${val}&api-key=${apiKey}`
-    console.log(queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-}
+    function getNYTarticles(val) {
+        var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${val}&api-key=${apiKey}`
+        console.log(queryURL)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
+});
