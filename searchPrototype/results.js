36137ab3171f4891cbfd2243e45cce7826796a96
Svitlana Zaster <szaster@protonmail.com>
2020-05-26 20:52:32 -0500

    Added a call to fetch function for NYTimes API

--- searchPrototype/results.js
+++ searchPrototype/results.js
@@ -1,7 +1,26 @@
-// This function loads results
+const nyTimes = {
+    mostPolular: "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json",
+}
 
+function withKey(url) {
+    const key = apiKeys.nyTimes;
+    return `${url}?api-key=${key}`;
+}
+
+// This function loads results
 function onWindowLoad() {
-console.log("I'm working")
+    console.log("I'm working")
+    
+    // passing nyTimes most popular as url into withKey function and then
+    // passing the result to fetch
+    fetch( withKey(nyTimes.mostPolular) );
+
+    if (apiKeys !== undefined) {
+        console.log("Environment:", apiKeys);
+    } else {
+        console.log("Api keys are not loaded");
+    }
+    
     const searchParams = new URLSearchParams(window.location.search);
     const text = searchParams.get('query');
     if (text) {
